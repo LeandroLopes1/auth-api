22e65e9135b6f93fb46bd25618256a8d5c367dd2
Daniel Rosendo <daniel.rosendos@hotmail.com>
2022-06-29 15:47:40 -0300

    subindo

--- src/core/middleware/response_middleware.py
+++ src/core/middleware/response_middleware.py
@@ -1,5 +1,7 @@
 import json
+from django import http
 from datetime import datetime
+from django.conf import settings
 from ..utils.response_utils import get_response
 from django.utils.deprecation import MiddlewareMixin
 from djangorestframework_camel_case.render import camelize
@@ -9,6 +11,17 @@ class ResponseMiddleware(MiddlewareMixin):
         request.start_time = datetime.now()
 
     def process_response(self, request, response):
+        debug = settings.DEBUG
+
+        if response.status_code == 404:
+            if debug:
+                return http.HttpResponse(
+                    "<h1>404 Not Found</h1>",
+                    content_type="text/html"
+                )
+
+            return response
+
         response.content = json.dumps(
             camelize(get_response(request, response)),
             sort_keys=True,
